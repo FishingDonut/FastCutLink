360848c3097b780e54b752e5252ecf442d4a52fb
FishingDonut <protronsdimi@gmail.com>
2025-02-05 17:16:02 -0300

    cria o link

--- app/api/link/create/route.ts
+++ app/api/link/create/route.ts
@@ -1,10 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/prisma/client";
+import { randomUUID } from "crypto";
 
 async function handler(req: NextRequest, res: NextResponse) {
     try{
         const data = await req.json();
-    
+        
+        const response = await fetch(data.fullLink);
+        
+        if(!response.ok){
+            return NextResponse.json("Link invalido", {status: 400}) ;
+        }
+
+        data["shortLink"] = Math.random().toString(36).substring(2, 2 + 6);
+
         const prismaResponse = await prisma.link.create({data: data});
         return NextResponse.json(prismaResponse);
     } catch (error){
