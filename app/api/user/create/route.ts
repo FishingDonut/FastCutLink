dbaa68c4091f67750655e48947ec1f8ac25e7909
FishingDonut <protronsdimi@gmail.com>
2025-02-06 14:20:07 -0300

    register ok

--- app/api/user/create/route.ts
+++ app/api/user/create/route.ts
@@ -1,15 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/prisma/client";
+import { Prisma } from "@prisma/client";
 
-async function handler(req: NextRequest, res: NextResponse) {
-    try{
-        const data = await req.json();
-    
-        const prismaResponse = await prisma.user.create({data: data});
-        return NextResponse.json(prismaResponse);
-    } catch (error){
+interface Idata {
+    email: string,
+    password: string,
+    passwordConfirm?: string
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+    try {
+        const { email, password }: Idata = await req.json();
+
+        const findUser = await prisma.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+        if(findUser){
+            return NextResponse.json({message: "Erro, email ou senha invalido"}, {status: 400});
+        }
+
+        const prismaResponse = await prisma.user.create({
+            data: {
+                email: email, password: password
+            }
+        });
+
+        return NextResponse.json(prismaResponse, {status: 201});
+    } catch (error) {
         return NextResponse.json(error);
     }
 }
-
-export { handler as POST };
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -33,7 +33,7 @@ export default function page() {
 
     const handleRegisterFormSuccess = async (data: RegisterSchema) => {
         try {
-            const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "user", {
+            const response = await fetch("api/user/create", {
                 method: "POST",
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(data)
