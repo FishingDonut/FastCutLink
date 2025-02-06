cce29cc6614f558848bf7f06291c3111f0995658
FishingDonut <protronsdimi@gmail.com>
2025-02-06 17:41:25 -0300

    update /

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import PaidIcon from '@mui/icons-material/Paid';
 
 export default function Home() {
   return (
@@ -6,46 +7,39 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/image/logo2.png"
+          alt="FastCultLink logo"
           width={180}
           height={38}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Crie uma conta e faça Login{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
+              /login
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Acesse o painel para gerenciar os links.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="/register"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+          >  
+            Cadastro
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="/login"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Login
           </a>
         </div>
       </main>
@@ -63,7 +57,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Terms
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -78,22 +72,19 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          Dashboard
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
+          <PaidIcon
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Donate Pix
         </a>
       </footer>
     </div>
