614fd01d39c066149dce570152e094ded06a72ae
FishingDonut <protronsdimi@gmail.com>
2025-02-10 15:50:17 -0300

    create / delete

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,10 @@
 'use client';
 
-import { Grid2 as Grid, Typography, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Grid2 as Grid, Typography, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Button, Modal, Box, Paper, FormControl, Input } from "@mui/material";
+import SaveIcon from '@mui/icons-material/Save';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import AddIcon from '@mui/icons-material/Add';
+import { FormEvent, useEffect, useState } from "react";
 import UseGetLinks from "./UseGetLinks";
 import { useSession } from "next-auth/react";
 import { Link } from "@prisma/client";
@@ -14,9 +17,19 @@ const formatDate = (dateString: string | Date) => {
     return format(date, "dd/MM/yyyy", { locale: ptBR });
 }
 
+const onDelete = async (id: number) => {
+    await fetch("api/link/delete", {
+        method: "DELETE",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id: id })
+    });
+}
+
+
 export default function Page() {
     const { data: session, status } = useSession();
     const [links, setlinks] = useState<Link[] | null>(null);
+    const [open, setOpen] = useState<boolean>(false);
 
     const titles = ["Id", "Name", "FullLink", "ShortLink", "Date"];
 
@@ -50,14 +63,37 @@ export default function Page() {
             }
         }
         fetchData();
-    }, [router, status, session])
+    }, [router, status, session, onDelete])
+
+    const onCreateUserLink = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+
+        const { name, fullLink } = await { name: formData.get("name"), fullLink: formData.get("fullLink") }
+        
+        await fetch("api/link/create", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                userId: session?.user.id,
+                name: name,
+                fullLink: fullLink
+            })
+        });
+    }
 
     return (
         <Grid container display="flex" flexDirection="column" alignContent="center">
-            <Grid>
-                <Typography variant="h3" fontWeight="bold" color="primary">
-                    Seus Links
-                </Typography>
+            <Grid container display="flex" alignContent="center">
+                <Grid size={1}>
+                </Grid>
+                <Grid size={10}>
+                    <Typography variant="h3" align="center" margin={1} fontWeight="bold" color="primary">
+                        Seus Links
+                    </Typography>
+                </Grid>
+                <Grid size={1}>
+                </Grid>
             </Grid>
             <Grid margin={0} padding={0} spacing={0} size={1}></Grid>
             <Grid size={10} margin={1}>
@@ -68,10 +104,15 @@ export default function Page() {
                                 {
                                     titles.map((title) => (
                                         <TableCell align="left">
-                                            <Typography fontWeight="bold" color="primary.dark">{title}</Typography>
+                                            <Typography fontWeight="bold" color="secondary">{title}</Typography>
                                         </TableCell>
                                     ))
                                 }
+                                <TableCell>
+                                    <Button variant="outlined" onClick={() => { setOpen(true) }}>
+                                        <AddIcon />
+                                    </Button>
+                                </TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
@@ -83,8 +124,8 @@ export default function Page() {
                                                 <Typography color="secondary">{index + 1}</Typography>
                                             </TableCell>
                                             <TableCell>
-                                            <Typography fontWeight="bold">{link.name}</Typography>
-                                                </TableCell>
+                                                <Typography fontWeight="bold">{link.name}</Typography>
+                                            </TableCell>
                                             <TableCell>
                                                 <a href={link.fullLink}>
                                                     {link.fullLink}
@@ -92,6 +133,9 @@ export default function Page() {
                                             </TableCell>
                                             <TableCell>{link.shortLink}</TableCell>
                                             <TableCell>{formatDate(link.createdAt)}</TableCell>
+                                            <TableCell>
+                                                <Button onClick={() => { onDelete(link.id) }} color="warning" variant="contained"><DeleteForeverIcon className="dark:invert" /></Button>
+                                            </TableCell>
                                         </TableRow>
                                     ))
                                 )
@@ -101,6 +145,43 @@ export default function Page() {
                 </TableContainer>
             </Grid>
             <Grid margin={0} padding={0} spacing={0} size={1}></Grid>
+            <Modal open={open} onClose={() => { setOpen(false) }}>
+                <Box
+                    padding={2}
+                    component={Paper}
+                    bgcolor="ActiveBorder"
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                    position="absolute"
+                    top="50%"
+                    left="25%"
+                    right="25%"
+                >
+                    <Grid size={12} id="title-modal">
+                        <Typography variant="h2" color="primary" fontWeight="bold">Criar Link</Typography>
+                    </Grid>
+                    <Grid margin={2} size={12} id="body">
+                        <form onSubmit={(event) => { onCreateUserLink(event) }}>
+                            <Grid margin={2} size={12}>
+                                <FormControl variant="standard">
+                                    <Input name="name" placeholder="Name"></Input>
+                                </FormControl>
+                            </Grid>
+                            <Grid margin={2} size={12}>
+                                <FormControl variant="standard">
+                                    <Input name="fullLink" placeholder="FullLink"></Input>
+                                </FormControl>
+                            </Grid>
+                            <Grid display="flex" alignItems="center" flexDirection="column" size={12}>
+                                <Button type="submit" onClick={() => { }} variant="contained" fullWidth>
+                                    <SaveIcon className="dark:invert" />
+                                </Button>
+                            </Grid>
+                        </form>
+                    </Grid>
+                </Box>
+            </Modal>
         </Grid >
     );
 }
