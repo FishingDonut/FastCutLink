01a610a9d41fe752cd50c87f0e07bcc3510820f5
FishingDonut <protronsdimi@gmail.com>
2025-02-10 20:47:05 -0300

    --vercel for update

--- app/api/user/find/route.ts
+++ app/api/user/find/route.ts
@@ -1,10 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/prisma/client";
-import { User } from "@prisma/client";
+import { Prisma} from "@prisma/client";
+
+type User = Prisma.UserGetPayload<{}>;
 
 async function handler(req: NextRequest) {
     try{
         const data = await req.json();
+
 
         const prismaResponse: User|null = await prisma.user.findFirst({where: {
             email: data.email
