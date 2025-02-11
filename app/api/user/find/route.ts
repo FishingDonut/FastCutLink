dce68fe9a3aedc69f876a749fcb763727f47adf9
FishingDonut <protronsdimi@gmail.com>
2025-02-11 11:58:56 -0300

    update

--- app/api/user/find/route.ts
+++ app/api/user/find/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/prisma/client";
-import { Prisma} from "@prisma/client";
+import { Prisma } from "@prisma/client";
 
 type User = Prisma.UserGetPayload<true>;
 
